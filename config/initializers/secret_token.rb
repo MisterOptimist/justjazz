e8d7cd93686c98eef95144179f92851ff5415b37
Casey Dierking <hnwebdesign5@gmail.com>
2013-09-04 08:10:22 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustJazz::Application.config.secret_token = '9c6047af10632b34b8df6d014d8d633b16bd8f53ee26633328bc338a76d8bf7c81850f6951bfaced1644254130dcc3337cc900f17ae8eee5344e4d01b395bc7c'
